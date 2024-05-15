93c36d47eff6bd5762148d7e3ee2d2b3b2910b55
Kuba <jakubzengota@gmail.com>
2024-05-15 20:29:40 +0200

    gabiKotek

---
+++ index.js
@@ -0,0 +1,5 @@
+function displayText() {
+    const textElement = document.getElementById('centered-text');
+    textElement.textContent = 'Hello, world!';
+    textElement.style.display = 'block';
+}
